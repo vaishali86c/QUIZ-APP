6290b33266eef58ca215df048f5be394a11d61e2
vaishali86c <vaishali86c@gmail.com>
2024-05-05 23:21:00 +0530

    Create exam schema and route for adding quiz

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares/authMiddleware");
+
 
 // user registration
 
@@ -35,6 +38,66 @@ router.post("/register", async (req, res) => {
         });
     }
 });
+
+
+// user login 
+
+router.post("/login", async (req, res) => {
+    try {
+      // check if user exists
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res
+                .status(200)
+                .send({ message: "User does not exist", success: false });
+            }
+  
+      // check password
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!validPassword) {
+            return res
+                .status(200)
+                .send({ message: "Invalid password", success: false });
+        }
+  
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d",
+        });
+  
+        res.send({
+            message: "User logged in successfully",
+            success: true,
+            data: token,
+        });
+        } catch (error) {
+            res.status(500).send({
+            message: error.message,
+            data: error,
+            success: false,
+        });
+    }
+});
+  
+
+// router.post("/get-user-info", authMiddleware, async (req, res) => {
+//     try {
+//         const user = await User.findById(req.body.userId);
+//             res.send({
+//                 message: "User info fetched successfully",
+//                 success: true,
+//                 data: user,
+//             });
+//     } catch (error) {
+//         res.status(500).send({
+//             message: error.message,
+//             data: error,
+//             success: false,
+//         });
+//     }
+// });
 
 
 
