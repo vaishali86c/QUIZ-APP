5a32e362a301ea39b134d554778382f68bac1beb
vaishali86c <vaishali86c@gmail.com>
2024-05-05 21:07:17 +0530

    Create backend folders and login route

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+
+
+app.use(express.json());
+
+const usersRoute = require("./routes/usersRoute");
+app.use("/api/users", usersRoute);
+
+
+const port = process.env.PORT || 5000;
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
